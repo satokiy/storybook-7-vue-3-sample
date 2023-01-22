c21046e0fd65f8bbb57058df7afe68bb80e529d7
satokiy <syamada@netprotections.co.jp>
2023-01-22 21:19:57 +0900

    [add]add pinia

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,20 @@
 import { Decorator, Parameters } from "@storybook/vue3";
 import i18n from "../src/i18n";
 import { setup } from "@storybook/vue3";
+import pinia, { useCurrentUserStore } from "../src/pinia";
 
 export const parameters: Parameters = {};
 export const decorators: Decorator[] = [
   (_story, context) => {
     i18n.global.locale = context.globals.locale;
-    return {template: '<story></story>'}
-
-  }
+    return {
+      setup() {
+        const currentUserStore = useCurrentUserStore();
+        currentUserStore.$reset();
+      },
+      template: "<story></story>",
+    };
+  },
 ];
 
 export const globalTypes = {
@@ -23,7 +29,8 @@ export const globalTypes = {
   },
 };
 setup((app) => {
-  if(!app.__VUE_I18N__) {
-    app.use(i18n)
+  if (!app.__VUE_I18N__) {
+    app.use(i18n);
+    app.use(pinia);
   }
-})
+});
